26579de29eec76bccb56a5772cd1e23eef5d8de6
YUMENOSUKE <yumejustice@gmail.com>
2026-07-15 21:46:24 +0900

    Update lyra-screensaver to 0.1.1

--- Casks/lyra-screensaver.rb
+++ Casks/lyra-screensaver.rb
@@ -1,8 +1,8 @@
 cask "lyra-screensaver" do
-  version "0.1.0"
-  sha256 "8d1038edbe85f74eea0dd63f15c4bf093a3d6b05e88be24caea3a5ec783fdd58"
+  version "0.1.1"
+  sha256 "7d9f298aee3cca6bf4952b982f0596edc28778e1897cf22ec49ce23c48df44b6"
 
-  url "https://github.com/GeneralD/lyra-screensaver/releases/download/v0.1.0/LyraScreenSaver-0.1.0.zip"
+  url "https://github.com/GeneralD/lyra-screensaver/releases/download/v0.1.1/LyraScreenSaver-0.1.1.zip"
   name "Lyra Screen Saver"
   desc "macOS screensaver that plays lyra's video wallpaper"
   homepage "https://github.com/GeneralD/lyra-screensaver"
