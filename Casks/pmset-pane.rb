cf3a72ea23dc7ea753a3af3dbd5a9b96cc9a3fd5
GeneralD <yumejustice@gmail.com>
2026-09-02 08:59:49 +0900

    feat: update Power Management to 0.1.1

--- Casks/pmset-pane.rb
+++ Casks/pmset-pane.rb
@@ -1,9 +1,9 @@
 cask "pmset-pane" do
-  version "0.1.0"
-  sha256 "a4a917f7e43a201b3143138e995dd20dbb2419cee49e5b28565c29cdbf42e56a"
+  version "0.1.1"
+  sha256 "757be377a861f67eb93ea410867a6bc529273c17a6670c1d93c77a3d96877b61"
 
   url "https://github.com/GeneralD/pmset-pane/releases/download/v#{version}/PMSetPane.zip"
-  name "PMSet Pane"
+  name "Power Management"
   desc "Preference pane for common pmset power-management settings"
   homepage "https://github.com/GeneralD/pmset-pane"
 
